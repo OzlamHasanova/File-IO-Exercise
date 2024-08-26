b010d822b466f68eaea0b8f8f8c9c3b083e6922c
OzlamHasanova <7qk9bme@code.edu.az>
2024-08-26 18:19:42 +0400

    add project requirements classe and enums

---
+++ src/Employee.java
@@ -0,0 +1,156 @@
+import java.time.LocalDate;
+import java.util.Comparator;
+
+public class Employee implements Comparator {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+    private double salary;
+    private Department department;
+    private boolean isEmployee;
+    private LocalDate startDate;
+    private String email;
+    private String phoneNumber;
+    private Position position;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public Employee(int id, String name, String surname, int age, double salary, Department department, boolean isEmployee, LocalDate startDate, String email, String phoneNumber, Position position, String address) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.salary = salary;
+        this.department = department;
+        this.isEmployee = isEmployee;
+        this.startDate = startDate;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.position = position;
+        this.address = address;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public boolean isEmployee() {
+        return isEmployee;
+    }
+
+    public void setEmployee(boolean employee) {
+        isEmployee = employee;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", department=" + department +
+                ", isEmployee=" + isEmployee +
+                ", startDate=" + startDate +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", position=" + position +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,72 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static List<Employee> employeeList=new ArrayList<>();
+
+    public static void main(String[] args){
+        readEmployee();
+        filterData();
+        sortForStartDate();
+        raiseSalarySomeEmployee();
     }
+    public static void readEmployee(){
+        //1. Read Employee Data from a Text File
+        try(BufferedReader bufferedReader=new BufferedReader(new FileReader("employees.txt"))){
+            String line;
+            while ((line=bufferedReader.readLine())!=null){
+                String[] elementList=line.split(",");
+                employeeList.add(new Employee(Integer.parseInt(elementList[0]),elementList[1],elementList[2],
+                        Integer.parseInt(elementList[3]),Integer.parseInt(elementList[4]),Department.valueOf(elementList[5]),
+                        Boolean.parseBoolean(elementList[6]), LocalDate.parse(elementList[7]),elementList[8],elementList[9],
+                        Position.valueOf(elementList[10]),elementList[11]));
+                System.out.println("-----------------------------------");
+                employeeList.stream().forEach(System.out::println);
+                System.out.println("-----------------------------------");
+
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void filterData() {
+        System.out.println("=============Filter out employees who are interns========");
+        List<Employee> employeesFilter = employeeList.stream().filter(employee ->
+                        Position.INTERN == (employee.getPosition()))
+                .collect(Collectors.toList());
+        if (employeesFilter.isEmpty()) {
+            System.out.println("No intern");
+        } else {
+             employeesFilter.forEach(System.out::println);
+        }
+    }
+    public static void sortForStartDate() {
+        System.out.println("=============Sort the employees by their start date========");
+        employeeList.stream().sorted(Comparator.comparing(Employee::getStartDate)).forEach(System.out::println);
+    }
+    public static void raiseSalarySomeEmployee(){
+        System.out.println("=============Give a 10% salary raise to employees in the IT department with more than 5 years of experience========");
+        List<Employee> employees=employeeList.stream().filter(employee ->
+                        Period.between(employee.getStartDate(),LocalDate.now()).getYears()>5)
+                .map(employee -> {employee.setSalary(employee.getSalary()+ employee.getSalary()*0.1);
+                return employee;
+                }).toList();
+        if (employees.isEmpty()) {
+            System.out.println("Does not have employee in the IT department with more than 5 years of experience");
+        } else {
+            employees.forEach(System.out::println);
+        }
+    }
+
+
+
 }
